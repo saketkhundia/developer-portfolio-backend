0997e6e7d67cc916c818529b3f6cab52ca13d7b0
saketkhundia <saketkhundia21@gmail.com>
2026-03-06 10:48:04 +0530

    Backend Updated

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["https://deviq-bay.vercel.app", "http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
